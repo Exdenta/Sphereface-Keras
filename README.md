00f7d414c36c46bcc8077a6c2efc9569736642dd
Lex Sher <alexandershershakov@gmail.com>
2020-05-06 22:34:58 +0300

    preprocessing code and caffe model

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 A Keras Implementation of SphereFace.
 
 [SphereFace: Deep Hypersphere Embedding for Face Recognition](https://arxiv.org/abs/1704.08063)
+
+Pretrained models: 
+- Original Sphereface-20 Caffe model from [wy1iu/sphereface](https://github.com/wy1iu/sphereface): [link](https://drive.google.com/file/d/0B_geeR2lTMegb2F6dmlmOXhWaVk/view)
--- test/evaluation.py
+++ test/evaluation.py
@@ -1,104 +1,167 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import os
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-import os
-os.environ['GLOG_minloglevel'] = '2' # suppress Caffe verbose prints
+os.environ['GLOG_minloglevel'] = '2'  # suppress Caffe verbose prints
 
-import sys
-import cv2
-import math
-import time
-import caffe
-import numpy as np
-from tqdm import tqdm
-from pathlib import Path
-from scipy import spatial
 from datetime import datetime
+from scipy import spatial
+from pathlib import Path
+import multiprocessing
+from tqdm import tqdm
+import numpy as np
+import caffe
+import time
+import math
+import cv2
+import sys
 
-def get_embeddings(net, image):
-    image = (image - 127.5) / 128
-    image = np.asarray(image)
-    image = np.transpose(image, (2,0,1))
-    image = image[None, :]
-    net.blobs['data'].data[...] = image
-    out = net.forward()
-    return out['fc5'][0]
+current_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+project_path = current_dir.parent
 
+process_count = 6
+flip_image = False # will add embeddings of the flipped image also
+model_path = project_path / 'models' / 'sphereface_20_caffe' / 'sphereface_deploy.prototxt'
+weights_path = project_path / 'models' / 'sphereface_20_caffe' / 'sphereface_model.caffemodel'
+dataset_path = project_path / 'test' / 'data'
+list_path = dataset_path / 'pairs.txt'
+folder_path = dataset_path / 'lfw_112X96'
 
-def getAccuracy(pairs, threshold):
-    TP = sum(map(lambda x: x['score']>threshold and x['flag']==1, pairs))
-    TN = sum(map(lambda x: x['score']<threshold and x['flag']!=1, pairs))
-    return (TP + TN) / len(pairs)
+caffe.set_mode_cpu()
 
-
-if __name__ == "__main__":
-    current_dir = Path(os.path.dirname(os.path.realpath(__file__)))
-    project_path = current_dir.parent.parent
-    dataset_path = os.path.join(project_path, 'test/data')
-    list_ = os.path.join(dataset_path, 'pairs.txt')
-    folder_ = os.path.join(dataset_path, 'lfw-112X96')
-
+def form_features(pair):
+    def get_embeddings(net, image):
+        image_ = (image - 127.5) / 128
+        image_ = np.asarray(image_)
+        image_ = np.transpose(image_, (2, 0, 1))
+        image_ = image_[None, :]
+        net.blobs['data'].data[...] = image_
+        out = net.forward()
+        return out['fc5'][0]
+    
     # init model
-    caffe.set_mode_cpu()
-    model   = os.path.join(project_path, 'train/code/sphereface_deploy.prototxt')
-    weights = os.path.join(project_path, 'train/result/sphereface_model.caffemodel')
-    net = caffe.Net(model, weights, caffe.TEST)
+    net = caffe.Net(str(model_path), str(weights_path), caffe.TEST)
+
+    # featureL
+    imageL = cv2.imread(str(pair['fileL']))
+    embsL = get_embeddings(net, imageL)
+    if flip_image:
+        imageL_flipped = cv2.flip(imageL, 1)
+        embsL_flipped = get_embeddings(net, imageL_flipped)
+        embsL = np.concatenate((embsL, embsL_flipped))
+    pair['featureL'] = embsL.copy()
+
+    # featureR
+    imageR = cv2.imread(str(pair['fileR']))
+    embsR = get_embeddings(net, imageR)
+    if flip_image:
+        imageR_flipped = cv2.flip(imageR, 1)
+        embsR_flipped = get_embeddings(net, imageR_flipped)
+        embsR = np.concatenate((embsR, embsR_flipped))
+    pair['featureR'] = embsR.copy()
+
+    # cosine distance
+    pair['score'] = 1 - spatial.distance.cosine(pair['featureL'], pair['featureR'])
+    
+    return pair
+
+
+def create_pairs():
+    """ Creates list of pairs from pairs.txt and saves them to pairs_*.npy
+        Each pair is a map object with fields:
+            fileL    - path to the first image
+            fileR    - path to the second image
+            flag     - "1" if first and second images are of the same person, "-1" otherwise
+            fold     - value from 1 to 10, denotes section of the lfw dataset
+            featureL - feature vector of the first image (model output, 512 features)
+            featureR - feature vector of the first image (model output, 512 features)
+            score    - cosine distance between featureL and featureR
+    """
+
+    # check paths do exist
+    paths = [list_path, folder_path]
+    for path in paths:
+        if not path.exists():
+            exit("Path {} doesn't exist!".format(path))
 
     # get features
     pairs = []
-    with open(list_, 'r') as f:
+    with open(str(list_path), 'r') as f:
         lines = f.readlines()
-    
-    print("Forming features:")
-    for i, line in enumerate(tqdm(lines)):
+
+    for i, line in enumerate(lines):
         if i < 1:
             continue
         pair = {}
         l = line.split()
         if len(l) == 3:
-            pair['fileL'] = os.path.join(folder_, l[0], l[0] + "_" + l[1].rjust(4, '0') + ".jpg")
-            pair['fileR'] = os.path.join(folder_, l[0], l[0] + "_" + l[2].rjust(4, '0') + ".jpg")
-            pair['flag']  = 1
+            # recreating relative path + file name
+            pair['fileL'] = folder_path / l[0] / (l[0] +
+                                                  "_" + l[1].rjust(4, '0') + ".jpg")
+            pair['fileR'] = folder_path / l[0] / (l[0] +
+                                                  "_" + l[2].rjust(4, '0') + ".jpg")
+            pair['flag'] = 1
         elif len(l) == 4:
-            pair['fileL'] = os.path.join(folder_, l[0], l[0] + "_" + l[1].rjust(4, '0') + ".jpg")
-            pair['fileR'] = os.path.join(folder_, l[2], l[2] + "_" + l[3].rjust(4, '0') + ".jpg")
-            pair['flag']  = -1
-        pair['fold']  = math.ceil(i / 600)
-        # featureL
-        image = cv2.imread(pair['fileL'])
-        embsL = get_embeddings(net, image)
-        pair['featureL'] = embsL.copy()
-        # featureR
-        image   = cv2.imread(pair['fileR'])
-        embsR   = get_embeddings(net, image)
-        pair['featureR'] = embsR.copy()
-        # cosine distance
-        pair['score'] = 1 - spatial.distance.cosine(pair['featureL'], pair['featureR']) 
-        pairs.append(pair)
+            pair['fileL'] = folder_path / l[0] / (l[0] +
+                                                  "_" + l[1].rjust(4, '0') + ".jpg")
+            pair['fileR'] = folder_path / l[2] / (l[2] +
+                                                  "_" + l[3].rjust(4, '0') + ".jpg")
+            pair['flag'] = -1
+        pair['fold'] = math.ceil(i / 600)
+        if pair['fileL'].exists() and pair['fileR'].exists():
+            pairs.append(pair)
+
+    print("Process images:")
+    pool = multiprocessing.Pool(processes=process_count)
+    pbar = tqdm(total=len(pairs))
+
+    def update(*a):
+        pbar.update()
+
+    async_requests = [pool.apply_async(form_features, args=(pair,), callback=update) for pair in pairs]
+    pool.close()
+    pool.join()
+    pairs = [request.get() for request in async_requests]
+
+    return pairs
+
+
+def get_accuracy(pairs, threshold):
+    TP = sum(map(lambda x: x['score'] > threshold and x['flag'] == 1, pairs))
+    TN = sum(map(lambda x: x['score'] < threshold and x['flag'] != 1, pairs))
+    return (TP + TN) / len(pairs)
+
+
+if __name__ == "__main__":
+    # process images from pairs.txt
+    pairs = create_pairs()
 
     # save for future testing
-    np.save("test/code/pairs_{}.npy".format(datetime.now().strftime("%Y%m%d%H%M%S")), pairs)
+    pairs_path = current_dir / 'data' / 'pairs.npy'
+    np.save(str(pairs_path), pairs)
+    
+    # load saved file
+    pairs = np.load(str(pairs_path), allow_pickle=True)
 
-    # get accuracy
-    print("Accuracy:")
+    print("\nAccuracy:")
     ACCs = np.zeros(10)
     Thrs = np.zeros(10)
     for j in np.arange(0, 10):
-        validation_pairs = list(filter(lambda x: x['fold']!=j+1, pairs))
-        test_pairs = list(filter(lambda x: x['fold']==j+1, pairs))
+        validation_pairs = list(filter(lambda x: x['fold'] != j + 1, pairs))
+        test_pairs = list(filter(lambda x: x['fold'] == j + 1, pairs))
 
         thrNum = 1000
-        accuracys  = np.zeros(2*thrNum)
+        accuracys = np.zeros(2*thrNum)
         thresholds = np.arange(-thrNum, thrNum) / thrNum
         for i in np.arange(0, 2*thrNum):
-            accuracys[i] = getAccuracy(validation_pairs, thresholds[i])
-        
+            accuracys[i] = get_accuracy(validation_pairs, thresholds[i])
+
         threshold = np.mean(thresholds[np.where(accuracys == max(accuracys))])
-        ACCs[j] = getAccuracy(test_pairs, threshold)
+        ACCs[j] = get_accuracy(test_pairs, threshold)
         Thrs[j] = threshold
-        print("accuracy: ", ACCs[j] * 100, ", threshold: ", threshold)
+        print("fold: ", j + 1, ", accuracy: ", ACCs[j] * 100, ", threshold: ", threshold)
 
     print('----------------')
     print('AVE: ', np.mean(ACCs)*100)
